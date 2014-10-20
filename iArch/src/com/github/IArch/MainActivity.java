61776e2d2428e0d9100c7d994f4f54938e97b678
Nick Weidner <weidnern@email.sc.edu>
2014-10-20 13:57:06 -0400

    Added a simple button that allows the user to log into dropbox

--- iArch/src/com/github/IArch/MainActivity.java
+++ iArch/src/com/github/IArch/MainActivity.java
@@ -1,16 +1,62 @@
 package com.github.IArch;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.dropbox.sync.android.DbxAccount;
+import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxDatastoreManager;
+import com.dropbox.sync.android.DbxException;
 
 public class MainActivity extends ActionBarActivity {
 
+	static final int REQUEST_LINK_TO_DBX = 0;
+	
+	private Button mLinkButton;
+	private DbxAccountManager mAccountManager;
+	private DbxDatastoreManager mDatastoreManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mAccountManager = DbxAccountManager.getInstance(getApplicationContext(), "75909bbddyeyj2q", "ynmavrsx2o3g0c8");
+
+	    // Button to link to Dropbox
+	    mLinkButton = (Button) findViewById(R.id.link_button);
+	    mLinkButton.setOnClickListener(new OnClickListener() {
+	        @Override
+	        public void onClick(View v) {
+	            mAccountManager.startLink((Activity)MainActivity.this, REQUEST_LINK_TO_DBX);
+	        }
+	    });
+
+	    // Set up the datastore manager
+	    if (mAccountManager.hasLinkedAccount()) {
+	        try {
+	            // Use Dropbox datastores
+	            mDatastoreManager = DbxDatastoreManager.forAccount(mAccountManager.getLinkedAccount());
+	            // Hide link button
+	            mLinkButton.setVisibility(View.GONE);
+	        } catch (DbxException.Unauthorized e) {
+	            System.out.println("Account was unlinked remotely");
+	        }
+	    }
+	    if (mDatastoreManager == null) {
+	        // Account isn't linked yet, use local datastores
+	        mDatastoreManager = DbxDatastoreManager.localManager(mAccountManager);
+	        // Show link button
+	        mLinkButton.setVisibility(View.VISIBLE);
+	    }
+		
 	}
 
 	@Override
@@ -31,4 +77,28 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_LINK_TO_DBX) {
+	        if (resultCode == Activity.RESULT_OK) {
+	            DbxAccount account = mAccountManager.getLinkedAccount();
+	            try {
+	                // Migrate any local datastores to the linked account
+	                mDatastoreManager.migrateToAccount(account);
+	                // Now use Dropbox datastores
+	                mDatastoreManager = DbxDatastoreManager.forAccount(account);
+	                // Hide link button
+	                mLinkButton.setVisibility(View.GONE);
+	            } catch (DbxException e) {
+	                e.printStackTrace();
+	            }
+	        } else {
+	            // Link failed or was cancelled by the user
+	        }
+	    } else {
+	        super.onActivityResult(requestCode, resultCode, data);
+	    }
+	}
+	
 }
