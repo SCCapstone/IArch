bf2975357d1fda452fd80e7493fd8ccc887f8736
Nick Weidner <weidnern@email.sc.edu>
2014-12-02 20:46:39 -0500

    Unfinished Gallery for experimentation. Needs fixing still.

---
+++ IArch/src/com/github/IArch/Gallery.java
@@ -0,0 +1,53 @@
+package com.github.IArch;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.GridView;
+
+public class Gallery extends Activity {
+
+	private GridView gridView;
+	private GridViewAdapter customGridAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gallery);
+
+		gridView = (GridView) findViewById(R.id.gridView);
+		customGridAdapter = new GridViewAdapter(this, R.layout.row_grid, getData());
+		gridView.setAdapter(customGridAdapter);
+	}
+
+	private ArrayList getData() {
+		final ArrayList imageItems = new ArrayList();
+		// retrieve String drawable array
+		/*TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+		for (int i = 0; i < imgs.length(); i++) {
+			Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),
+					imgs.getResourceId(i, -1));
+			imageItems.add(new ImageItem(bitmap, "Image#" + i));
+		}*/
+		
+		File path = new File(Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES) + "/iArch/");
+	    File[] imageFiles = path.listFiles();/*
+	    for (int i = 0; i < imageFiles.length; i++) {
+	        Bitmap bitmap = BitmapFactory.decodeFile(imageFiles[i].getAbsolutePath());
+	        imageItems.add(new ImageItem(bitmap, "Image#" + i));
+	    }*/
+	    Bitmap bitmap = BitmapFactory.decodeFile(imageFiles[1].getAbsolutePath());
+        imageItems.add(new ImageItem(bitmap, "Image#" + 1));
+        bitmap = BitmapFactory.decodeFile(imageFiles[2].getAbsolutePath());
+        imageItems.add(new ImageItem(bitmap, "Image#" + 2));
+
+		return imageItems;
+
+	}
+}
