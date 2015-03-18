6ffb07461b8a81b067f67df90fef859438780267
kozmacm <kozmacm@email.sc.edu>
2015-03-18 11:22:18 -0400

    Covert main activity to an activity that contains a fragment container. Specify what to initially put into main fragment container. Getting everything to work again.

---
+++ IArch/src/com/github/IArch/MainFragment.java
@@ -0,0 +1,24 @@
+package com.github.IArch;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+
+	static Button mLinkButton;
+	
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mLinkButton = (Button) rootView.findViewById(R.id.link_button);
+        return rootView;
+    }
+}
